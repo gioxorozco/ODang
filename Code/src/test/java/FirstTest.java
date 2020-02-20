b3df30454df3aa03a56094e8af131a7224fab287
Marius Kleppe Larnøy <marius@Mariuss-MacBook-Air.local>
2020-02-19 16:14:08 -0800

    added base maven  structure

---
+++ Code/src/test/java/FirstTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class FirstTest {
+
+    @Test
+    public void ExampleTest() {
+        Assertions.assertEquals(2, 1+1);
+    }
+}
